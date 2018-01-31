41924d3898eb34a4c156dd042806663484763248
SilhDing <dingyihang1994@gmail.com>
2018-01-31 23:04:38 +0800

    modify app.py including jinja2 and middleware

--- www/app.py
+++ www/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__authout__ = 'Yihang Ding'
+__author__ = 'Yihang Ding'
 
 '''
 web application based on aiohttp
@@ -12,17 +12,122 @@ import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
-def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+import orm
+from jinja2 import Environment,FileSystemLoader
+from coroweb import add_routes, add_static
+
+def init_jinja2(app, **kw):
+	logging.info('init jinja2...')
+	options = dict(
+		autoescape = kw.get('autoescape', True),
+		block_start_string = kw.get('block_start_string', '{%'),
+		block_end_string = kw.get('block_end_string', '%}'),
+		variable_start_string = kw.get('variable_start_string', '{{'),
+		variable_end_string = kw.get('variable_end_string', '}}'),
+		auto_reload = kw.get('auto_reload', True)
+		#在Jinja2模板中，用{{ name }}表示一个需要替换的变量,用{% ... %}表示指令
+	)
+	path = kw.get('path', None)
+	if path is None:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+		# __file__ means the module file itself
+	logging.info('set jinja2 template path: %s' % path)
+	env = Environemnt(loader=FileSystemLoader(path), **options)
+	filters = kw.get('filters', None)
+	if filters is not None:
+		for name, f in filters.items():
+			env.filters[name] = f
+	app['__templating__'] = env
+
+# middleware #1: logging_factory: to log info of urls before handler.
+async def logger_factory(app, handler):
+	async def logger(request):
+		logging.info('Request: %s %s' % (request.method, request.path))
+		# await asyncio.sleep(0.3)
+		return (await handler(request))
+		# after logging continue other tasks by calling handler().
+		# handler here is an instance but callable (see in coroweb.py).
+	return logger
+
+# middleware #2
+async def data_factory(app, handler):
+	async def parse_data(request):
+		if request.method == 'POST':
+			if request.content_type.startswith('application/json'):
+				request.__data__ = await request.json()
+				logging.info('request json: %s' % str(request.__data__))
+			elif request.content_type.startswith('application/x-www-form-urlencoded'):
+				request.__data__ = await request.post()
+				logging.info('request form: %s' % str(request.__data__))
+		return (await handler(request))
+	return parse_data
+
+# middleware #3: deal with output after handler
+async def response_factory(app, handler):
+	async def response(request):
+		logging.info('Response handler...')
+		r = await handler(request)
+		# wait until handler has finished tasks
+		# then this middleware will make output as standardized: web.Response(...)
+		if isinstance(r, web.StreamResponse):
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body = r)
+			resp.content_type = 'application/octet_stream'
+			return resp
+		if isinstance(r, str):
+			if r.startswith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		if instance(r, dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else:
+				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		if isinstance(r, int) and r >= 100 and r < 600:
+			return web.Response(r)
+		if isinstance(r, tuple) and len(r) == 2:
+			t, m = r
+			if isinstance(t, int) and t >= 100 and t < 600:
+				return web.Response(t, str(m))
+		# default
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+
+def datetime_filter(t):
+	print(int(time.time()))
+	delta = int(time.time() - t)
+	if delta < 60:
+		return u'%s秒前' % (delta)
+	if delta < 3600:
+		return u'%s分钟前' % (delta // 60)
+	if delta < 86400:
+		return u'%s小时前' % (delta // 3600)
+	if delta < 604800:
+		return u'%s天前' % (delta // 86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 #please make sure that the port is 9000
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('Get', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+	await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
+	pp = web.Application(loop=loop, middleware = [logger_factory, response_factory])
+	init_jinja2(app, filters = dict(datetime = datetime_filter))
+	add_routes(app, 'handlers')
+	add_static(app)
+	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+if __name__ == '__main__':
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(init(loop))
+	loop.run_forever()
