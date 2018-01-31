5604936551c5b56feca3b4efaecb285d8f00e71b
SilhDing <dingyihang1994@gmail.com>
2018-02-01 00:07:54 +0800

    add configs files

---
+++ www/config.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Configurations.
+'''
+
+__author__ = 'Yihang Ding'
+
+import config_default
+
+class Dict(dict):
+	def __init__(self, names=(), values=(), **kw):
+		super().__init__(**kw)
+		for k, v in zip(names, values):
+			self[k] = v
+		
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+def merge(defaults, override):
+	r = {}
+	for k, v in defaults.items():
+		if k in override:
+			if isinstance(v, dict):
+				r[k] = merge(v, override[k])
+			else:
+				r[k] = override[k]
+		else:
+			r[k] = v
+	return r
+
+def toDict(d):
+	# turn dict object into Dict object
+	D = Dict()
+	for k, v in d.items():
+		D[k] = toDict(v) if isinstance(v, dict) else v
+	return D
+
+configs = config_default.configs
+
+try:
+	import config_override
+	configs = merge(configs, config_override.configs)
+except ImportError:
+	pass
+
+configs = toDict(configs)
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Yihang Ding'
+
+configs = {
+	'db': {
+		'host': '127.0.0.1'
+	}
+}
