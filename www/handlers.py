b9054f5b6538d6197686d506f9cdf0439fe4f85e
SilhDing <dingyihang1994@gmail.com>
2018-02-02 00:02:18 +0800

    add css and js frame; edit index()

--- www/handlers.py
+++ www/handlers.py
@@ -12,8 +12,11 @@ from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 @get('/')
-async def index(request):
-	logging.info('*** start to handler request by index()...')
-	users = await User.findAll()
-	logging.info('~~~~~~ %s'% users)
-	return {'__template__': 'test.html','users': users}
+def index(request):
+	summary = '对于复杂的HTML前端页面来说，我们需要一套基础的CSS框架来完成页面布局和基本样式。另外，jQuery作为操作DOM的JavaScript库也必不可少。'
+	blogs = [
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+		Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+		]
+	return {'__template__': 'blogs.html', 'blogs':blogs}
